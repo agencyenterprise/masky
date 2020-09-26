39aecebaf0f8869ad2f0763350230ef4e7cce673
Will Ockelmann-Wagner <will@ae.studio>
2020-09-25 17:17:26 -0700

    Updates

---
+++ precache-manifest.2b0a1ab9a9b30e06e4d2ec5992184a63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c0eb320fdf08620afc17193113ac5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aa0b9bbcf673a1a4d5e",
+    "url": "/static/js/2.87cc2d3e.chunk.js"
+  },
+  {
+    "revision": "46b99eb97c91d12fae637dd402ed6c6b",
+    "url": "/static/js/2.87cc2d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5280e5aa321de63ef4e5",
+    "url": "/static/js/main.f2afac86.chunk.js"
+  },
+  {
+    "revision": "3bdd7325464128ab6edf",
+    "url": "/static/js/runtime-main.b07709cd.js"
+  },
+  {
+    "revision": "95f04a288f1b813d73c00e09fff0b21a",
+    "url": "/static/media/corona.95f04a28.svg"
+  },
+  {
+    "revision": "9599ae062d045d9ba4aaec8371403d2b",
+    "url": "/static/media/health.9599ae06.svg"
+  },
+  {
+    "revision": "0a98c8a5231136b4a7b0fdb016d0526a",
+    "url": "/static/media/logo.0a98c8a5.svg"
+  }
+]);
